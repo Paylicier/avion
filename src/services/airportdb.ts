aae15201b530221707abdd57eaffd3e6cf61366d
Paul <paylicier@gmail.com>
2026-08-17 15:41:57 +0000

    fix: sshhhh

--- src/services/airportdb.ts
+++ src/services/airportdb.ts
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://airportdb.io/api/v1/';
-const API_KEY = "3538c6f223444e3aef8deab4d9f2848774beb8f5944fd1404d1bf7bc36c0e5e36d394e9fc95a63126d65e0aeda2e9b18";
+const API_KEY = "";
 
 export const getAirport = async (icaoCode: string): Promise<any | null> => {
     const url = `${BASE_URL}airport/${icaoCode}?apiToken=${API_KEY}`;
